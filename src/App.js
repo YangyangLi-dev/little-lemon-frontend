664ef07408ae347624c86e86d3089294dd0f9b6f
liyy <liyyco@126.com>
2023-05-29 15:13:48 +0800

    update package and index page

--- src/App.js
+++ src/App.js
@@ -1,53 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import Navbar from './components/Navbar';
+import Routing from './components/Routing';
+import Footer from './components/Footer';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0);
-  function handleClick(){
-    setCount(count+1);
-  }
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <body>
-        <MyButton count={count} onClick={handleClick}/>
-        <MyButton count={count} onClick={handleClick}/>
-      </body>
-     
-    </div>
+    <>
+      <Navbar/>
+      <Routing />
+      <Footer />
+    </>
   );
 }
-
-// function MyButton(){
-//   const [count, setCount] = useState(0);
-
-//   function handleClick(){
-//     setCount(count+1);
-//   }
-//   return (<button onClick={handleClick}>Clicked {count} times</button>)
-// }
-
-function MyButton({ count, onClick}){
-  return (
-    <button onClick={onClick}>
-      Clicked {count} times
-    </button>
-  );
-}
-
-export default App;
